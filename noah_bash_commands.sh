e520d37a83048209a5f40e2f3a7ed0c56e35b255
n0aht <n.telerman@gmail.com>
2023-10-27 20:41:17 -0400

    some comments

--- noah_bash_commands.sh
+++ noah_bash_commands.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#custom functions for paninski lab server
+
+#navigate to dev folder
 function cddev(){
 	cd ~/code/github/
 
@@ -11,9 +14,10 @@ function jupy_instance(){
 	jupyter lab --no-browser --ip 0.0.0.0
 }
 
+#opens a detatched screen running a hupyter lab instance accessible to the internet in the correct directory and conda env 
 function jupy(){
 	
-	cd ~/code/
+	cd ~/code/;
 	conda activate jupy
 	screen -dmS "jupy_screen" jupyter lab --no-browser --ip 0.0.0.0
 	conda deactivate
