eb6eafaf1755e102286eaa95c6d80836299c6e67
n0aht <n.telerman@gmail.com>
2023-10-27 22:24:10 -0400

    test

--- noah_bash_commands.sh
+++ noah_bash_commands.sh
@@ -14,7 +14,7 @@ function cddev(){
 #very lazy way to keep on top of git commits and pushes
 function lazygit(){
 
-	TO_PUSH="0"
+	TO_PUSH="1"
 
 	git add .
 	echo "git add . [adding all files in repo]"
